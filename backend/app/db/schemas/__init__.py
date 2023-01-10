208ca58cbc4596a81045e2ddda51cee8b6d28f4d
marcosSuperteia <marcos.nascimento@superteia.com.br>
2023-01-10 16:10:48 -0300

    Query Raw Material Without Cost

--- backend/app/db/schemas/__init__.py
+++ backend/app/db/schemas/__init__.py
@@ -5,7 +5,7 @@ from .role import Role, RoleCreate, RoleInDB, RoleUpdate # noqa: F401, E261
 from .user_role import UserRole, UserRoleCreate, UserRoleInDB, UserRoleUpdate # noqa: F401, E261, E501
 from .permission import Permission, PermissionCreate, PermissionUpdate, PermissionInDB # noqa: F401, E261 E501
 from .role_permission import RolePermission, RolePermissionBase, RolePermissionInDB, RolePermissionCreate, RolePermissionUpdate # noqa: F401, E261 E501
-from .md_plant import Plant
+from .md_plant import Plant, PlantInDBBase
 from .market_segment import MarketSegmentSc, MarketSegmentAll
 from .md_material import Material
 from .material_espec_std import MaterialEspecStdSc
