438e6fa866e569c3c54c72a9be926b6db4d633a7
fansubsbrasil <fansubsbrasil@gmail.com>
2023-01-11 11:03:45 -0300

    feature tradução

--- frontend/src/views/tissue_month/index.vue
+++ frontend/src/views/tissue_month/index.vue
@@ -4,23 +4,34 @@
     <div class="col-12">
         <h3>{{$t('dtc.plant')}}</h3>
         <div class="p-inputgroup">
-            <Dropdown optionLabel="text" v-model="plantSelected" :options="plant" placeholder="Select a plant"  @click="getPlant"/>
+            <Dropdown optionLabel="text" v-model="plantSelected" :options="plant" :placeholder="$t('dtc.plant')"  @click="getPlant"/>
         </div>
     </div>
     <div class="col-12">
-        <h3>Year</h3>
+        <h3>{{$t('dtc.year')}}</h3>
         <div class="p-inputgroup">
-            <InputText placeholder="Year" v-model="year"/>
+            <InputText :placeholder="$t('dtc.year')" v-model="year"/>
         </div>
     </div>
     <div class="col-2 mt-3">
-        <Button label="Enviar" @click="getPlant" icon="pi pi-send" iconPos="right" ></Button>
+        <Button v-if="!progress" :label="$t('message.search')" @click="submit()" icon="pi pi-send" iconPos="right" ></Button>
+        <ProgressSpinner v-if="progress" style="width:50px;height:50px" strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" aria-label="Custom ProgressSpinner" />
     </div>
+
+
+    <div>
+        <DataTable :value="table" responsiveLayout="scroll">
+            <Column field="material" header="Code"></Column>
+        </DataTable>
+    </div>
+
     
 </div>
 </template>
 <script setup>
 import Dropdown from 'primevue/dropdown';
+import Column from "primevue/column";
+import ProgressSpinner from 'primevue/progressspinner';
 import axios from "axios";
 import { useDtcStore } from '../../store/dtc';
 import { ref, onMounted } from "vue";
@@ -28,15 +39,21 @@ const dtc = useDtcStore()
 const plant = ref([]);
 const plantSelected = ref([]);
 const year = ref([]);
-
+const table = ref([])
+const progress = ref(false)
 const getPlant = async () => {
   plant.value = await dtc.setPlant()
 };
 onMounted(async () => {
     getPlant()
-    getMkg()
-    getLine()
     const d = new Date()
     year.value = d.getFullYear()
 })
+const submit = async () =>{
+    progress.value = !progress.value 
+    const response = await axios.get("http://localhost:8000/api/v1/multselect/raw");    
+    console.log(response)          
+    table = response.data
+    progress.value = !progress.value 
+}
 </script>
