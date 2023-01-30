bfbde914678851aad4314416a02af5d92b5825f3
marcosSuperteia <marcos.nascimento@superteia.com.br>
2023-01-30 09:44:08 -0300

    update one level - compound

--- frontend/src/views/compound_and_fabric/index.vue
+++ frontend/src/views/compound_and_fabric/index.vue
@@ -26,26 +26,33 @@
         </div>
     </div>
     <div class="col-2 mt-3">
-        <Button :label="$t('message.search')" @click="getComponents" icon="pi pi-send" iconPos="right" ></Button>
+        <Button v-if="!progress" :label="$t('message.search')" @click="getComponents()" icon="pi pi-send" iconPos="right" ></Button>
+        <ProgressSpinner v-if="progress" style="width:50px;height:50px" strokeWidth="8" fill="var(--surface-ground)"
+                animationDuration=".5s" aria-label="Custom ProgressSpinner" />
     </div>
     <div class="col-12">
         <TableCuston :tableValue=table></TableCuston>
     </div>    
 </div>
-
+<Toast />
 </template>
 <script setup>
 import Dropdown from 'primevue/dropdown';
 import axios from "axios";
 import { useDtcStore } from '../../store/dtc';
 import { ref, onMounted } from "vue";
+import { useToast } from "primevue/usetoast";
 import TableCuston from './table/mass.vue'
+import ProgressSpinner from 'primevue/progressspinner';
+import Toast from 'primevue/toast';
 const dtc = useDtcStore()
 const plant = ref([]);
+const toast = useToast();
 const material = ref([]);
 const plantSelected = ref([]);
 const materialSelected = ref([]);
 const year = ref([]);
+const progress = ref(false)
 const selectMaterialBreakdown= ref({ id: 1, text: 'Without Recycle' })
 const itemsMaterialBreakdown = ref([{ id: 1, text: 'Without Recycle' }, { id: 2, text: 'With Recycle (no explosion)' }, { id: 3, text: 'With Recycle (explosion)' }])
 const table = ref([]);
@@ -59,10 +66,12 @@ const getMassAll = async () => {
     material.value = dtc.mass_all 
 };
 const getComponents = async () => {
+    progress.value = true
     const response = await axios.get('http://localhost:8000/api/v1/compound_and_fabric?plant=' +plantSelected.value.value + '&material=' + materialSelected.value.value + '&year=' + year.value + '&type=' + selectMaterialBreakdown.value.id)
     console.log(response.data)
     table.value = response.data
-
+    toast.add({severity:'success', summary: 'Atualizado conforme solicitado', detail:'Permissoes atualizadas para a regra', life: 3000});
+    progress.value = false
 };
 
 onMounted(async () => {
